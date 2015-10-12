53f3288c4fdb0338a6be9b9dcdfb2fbc04a56bc6
theSage21 <arjoonn.94@gmail.com>
2015-10-12 11:44:27 +0530

    Perp update

--- perp.py
+++ perp.py
@@ -3,8 +3,9 @@ import socket
 
 program = '''
 import os
-print(os.listdir(os.getcwd()))
+print(os.environ)
 '''
+
 s = socket.create_connection(('127.0.0.1', 1024))
 s.sendall(program.encode('utf-8'))
 s.close()
